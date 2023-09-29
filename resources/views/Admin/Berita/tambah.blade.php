d5bf8d613ba0e742b941d7e9e94b0bad5b2c6cf5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-09-30 00:29:59 +0700

    Update tambah.blade.php

--- resources/views/Admin/Berita/tambah.blade.php
+++ resources/views/Admin/Berita/tambah.blade.php
@@ -28,12 +28,6 @@
                                 @endforeach
                             </select>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="berita_tanggal" class="form-label">Tanggal Pengajuan<span class="text-danger">*</span></label>
-                            <input type="text" name="berita_tanggal" class="form-control" value="{{ date('d-m-Y') }}" readonly>
-                        </div>
                         <div class="form-group">
                             <label for="berita_pihak_2" class="form-label">Pihak <b>KEDUA</b><span class="text-danger">*</span></label>
                             <select name="berita_pihak_2" class="select select-2 form-control">
@@ -42,6 +36,24 @@
                                 <option value="{{ $s->pegawai_id }}">{{ $s->nip }} - {{ $s->nama_lengkap }}</option>
                                 @endforeach
                             </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="berita_tanggal" class="form-label">Tanggal Pengajuan<span class="text-danger">*</span></label>
+                            <input type="text" name="berita_tanggal" class="form-control" value="{{ date('d-m-Y') }}" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="berita_header" class="form-label">Header Surat<span class="text-danger">*</span></label>
+                            <textarea type="text" name="berita_header" class="form-control" placeholder=""></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="berita_body" class="form-label">Body Surat<span class="text-danger">*</span></label>
+                            <textarea type="text" name="berita_body" class="form-control" placeholder=""></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="berita_footer" class="form-label">Footer Surat<span class="text-danger">*</span></label>
+                            <textarea type="text" name="berita_footer" class="form-control" placeholder=""></textarea>
                         </div>
                     </div>
                 </div>
