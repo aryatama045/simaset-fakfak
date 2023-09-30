1c8f8fbad462f51a29ad4a58b28625fd8af1b708
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-09-30 08:42:28 +0700

    Update index.blade.php

--- resources/views/Admin/Laporan/BarangMasuk/index.blade.php
+++ resources/views/Admin/Laporan/BarangMasuk/index.blade.php
@@ -45,19 +45,13 @@
                 <div class="table-responsive">
                     <table id="table-1" class="table table-bordered text-nowrap border-bottom dataTable no-footer dtr-inline collapsed">
                         <thead>
-                            <tr>
-                                <th class="border-bottom-0" rowspan="2" width="1%">No</th>
-                                <th class="border-bottom-0" rowspan="2">Tanggal Masuk</th>
-                                <th class="border-bottom-0" colspan="2">BARANG</th>
-                            </tr>
-                            <tr>
-                                <th class="border-bottom-0">Kode Barang Masuk</th>
-                                <th class="border-bottom-0">Kode Barang</th>
-                                <th class="border-bottom-0">Customer</th>
-                                <th class="border-bottom-0">Barang</th>
-                                <th class="border-bottom-0">Jumlah Masuk</th>
-                            </tr>
-                            
+                            <th class="border-bottom-0" width="1%">No</th>
+                            <th class="border-bottom-0" rowspan="2">Tanggal Masuk</th>
+                            <th class="border-bottom-0">Kode Barang Masuk</th>
+                            <th class="border-bottom-0">Kode Barang</th>
+                            <th class="border-bottom-0">Customer</th>
+                            <th class="border-bottom-0">Barang</th>
+                            <th class="border-bottom-0">Jumlah Masuk</th>
                         </thead>
                         <tbody></tbody>
                     </table>
