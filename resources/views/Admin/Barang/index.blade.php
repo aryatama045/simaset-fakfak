7824e6fa61bd5edc3142968ecf883aaf7fc4da9e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-01 11:33:30 +0700

    impot

--- resources/views/Admin/Barang/index.blade.php
+++ resources/views/Admin/Barang/index.blade.php
@@ -21,6 +21,7 @@
                 <h3 class="card-title">Data</h3>
                 @if($hakTambah > 0)
                 <div>
+                    <a class="modal-effect btn btn-success-light" onclick="generateID()" data-bs-effect="effect-super-scaled" data-bs-toggle="modal" href="#modalimport">Import <i class="fe fe-arrow-top"></i></a>
                     <a class="modal-effect btn btn-primary-light" onclick="generateID()" data-bs-effect="effect-super-scaled" data-bs-toggle="modal" href="#modaldemo8">Tambah Data <i class="fe fe-plus"></i></a>
                 </div>
                 @endif
@@ -53,6 +54,7 @@
 @include('Admin.Barang.edit', ['jenisbarang' => $jenisbarang, 'satuan' => $satuan, 'merk' => $merk])
 @include('Admin.Barang.hapus')
 @include('Admin.Barang.gambar')
+@include('Admin.Barang.import')
 
 <script>
     function generateID(){
