d9377d0bc38be0358e24f34af459aea1bff2e400
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-01 12:06:21 +0700

    Update copy.blade.php

--- resources/views/Admin/BarangMasuk/copy.blade.php
+++ resources/views/Admin/BarangMasuk/copy.blade.php
@@ -4,7 +4,7 @@
     <!-- PAGE-HEADER -->
     <div class="page-header">
         <h1 class="page-title">Tambah Barang Masuk<br><br>
-            <small>No. Pengadaan Barang : {{ $header[0]->pb_kode}}}</small></h1>
+            <small>No. Pengadaan Barang : {{ $header[0]->pb_kode }}</small></h1>
         <div>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item text-gray">Barang Masuk</li>
