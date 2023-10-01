900a2b4914c97ae28faabf18fa3f10f0d7c88536
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-01 17:30:17 +0700

    Update import.php

---
+++ resources/views/Admin/Barang/import.php
@@ -0,0 +1,83 @@
+<!-- MODAL TAMBAH -->
+<div class="modal fade" data-bs-backdrop="static" id="modalimport">
+    <div class="modal-dialog modal-md modal-dialog-centered" role="document">
+        <div class="modal-content modal-content-demo">
+            <div class="modal-header">
+                <h6 class="modal-title">Tambah Barang</h6><button onclick="reset()" aria-label="Close" class="btn-close" data-bs-dismiss="modal"><span aria-hidden="true">&times;</span></button>
+            </div>
+
+            <div class="modal-body">
+
+
+                <div class="form-group">
+                    <label for="title" class="form-label"> Import Data</label>
+                    <input class="form-control mt-5" id="GetFile" name="importbarang" type="file" onchange="VerifyFileNameAndFileSize()" accept=".csv,.xlsx,.xls">
+                </div>
+
+            </div>
+
+            <div class="modal-footer">
+                <button class="btn btn-primary d-none" id="btnLoader" type="button" disabled="">
+                    <span class="spinner-border spinner-border-sm me-1" role="status" aria-hidden="true"></span>
+                    Loading...
+                </button>
+                <a href="javascript:void(0)" onclick="checkForm()" id="btnSimpan" class="btn btn-primary">Simpan <i class="fe fe-check"></i></a>
+                <a href="javascript:void(0)" class="btn btn-light" onclick="reset()" data-bs-dismiss="modal">Batal <i class="fe fe-x"></i></a>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+@section('formTambahJS')
+<script>
+    function setLoading(bool) {
+        if (bool == true) {
+            $('#btnLoader').removeClass('d-none');
+            $('#btnSimpan').addClass('d-none');
+        } else {
+            $('#btnSimpan').removeClass('d-none');
+            $('#btnLoader').addClass('d-none');
+        }
+    }
+    function fileIsValid(fileName) {
+        var ext = fileName.match(/\.([^\.]+)$/)[1];
+        ext = ext.toLowerCase();
+        var isValid = true;
+        switch (ext) {
+            case 'csv':
+            case 'xlsx':
+            case 'xls':
+                break;
+            default:
+                this.value = '';
+                isValid = false;
+        }
+        return isValid;
+    }
+    function VerifyFileNameAndFileSize() {
+        var file = document.getElementById('GetFile').files[0];
+        if (file != null) {
+            var fileName = file.name;
+            if (fileIsValid(fileName) == false) {
+                validasi('Format bukan excel', 'warning');
+                document.getElementById('GetFile').value = null;
+                return false;
+            }
+            var content;
+            var size = file.size;
+            if ((size != null) && ((size / (1024 * 1024)) > 3)) {
+                validasi('Ukuran Maximum 1 MB', 'warning');
+                document.getElementById('GetFile').value = null;
+                return false;
+            }
+            var ext = fileName.match(/\.([^\.]+)$/)[1];
+            ext = ext.toLowerCase();
+            // $(".custom-file-label").addClass("selected").html(file.name);
+            document.getElementById('outputImg').src = window.URL.createObjectURL(file);
+            return true;
+        } else
+            return false;
+    }
+</script>
+@endsection
