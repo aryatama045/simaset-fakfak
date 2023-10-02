a5c59f52dbb06c2ebfc01a934e1b96cdc37e6c14
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-02 09:04:08 +0700

    Update BarangController.php

--- app/Http/Controllers/Admin/BarangController.php
+++ app/Http/Controllers/Admin/BarangController.php
@@ -373,6 +373,7 @@ class BarangController extends Controller
 
             $data= array_combine($escapedHeader, $columns);
 
+            dd($data);
             // if($data['jenis'])
                 $slug_jenis = strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $data['jenis'])));
                 $jenis_data = JenisBarangModel::firstOrCreate(['jenisbarang_nama' => $data['jenis'], 'jenisbarang_slug' => $slug_jenis, 'jenisbarang_ket' => '']);
