c9f1f66b4a3c3682a08c0fb38c6d994a1d9267c5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-05 16:43:18 +0700

    Update print.blade.php

--- resources/views/Admin/Laporan/StokBarang/print.blade.php
+++ resources/views/Admin/Laporan/StokBarang/print.blade.php
@@ -153,110 +153,112 @@ use Carbon\Carbon;
         <h4 class="font-medium">{{Carbon::parse($tglawal)->translatedFormat('d F Y')}} - {{Carbon::parse($tglakhir)->translatedFormat('d F Y')}}</h4>
         @endif
     </center> -->
-<div>
-    <header>
-        <div class="row">
-            <div id="img" class="col-md-3">
-                <!-- <img id="logo" src="https://getasanbersinar.files.wordpress.com/2016/02/logo-kabupaten-semarang-jawa-tengah.png" width="140" height="160" /> -->
 
-                @if($web->web_logo == '' || $web->web_logo == 'default.png')
-                <img id="logo" src="{{url('/assets/default/web/default.png')}}" width="80px" alt="">
-                @else
-                <img id="logo" src="{{url('/assets/default/web/default.png')}}" width="80px" alt="">
-                @endif
+    <div>
+        <header>
+            <div class="row">
+                <div id="img" class="col-md-3">
+                    <!-- <img id="logo" src="https://getasanbersinar.files.wordpress.com/2016/02/logo-kabupaten-semarang-jawa-tengah.png" width="140" height="160" /> -->
+
+                    @if($web->web_logo == '' || $web->web_logo == 'default.png')
+                    <img id="logo" src="{{url('/assets/default/web/default.png')}}" width="80px" alt="">
+                    @else
+                    <img id="logo" src="{{url('/assets/default/web/default.png')}}" width="80px" alt="">
+                    @endif
+                </div>
+                <div id="text-header" class="col-md-9">
+                    <h3 class="kablogo">PEMERINTAH KABUPATEN SEMARANG</h3>
+                    <h1 class="keclogo"><strong>KECAMATAN BERGAS</strong></h1>
+                    <h6 class="alamatlogo">Jl. Soekarno-Hatta, No. 68, Telepon/Faximile (0298) 523024</h6>
+                    <h5 class="kodeposlogo"><strong>BERGAS 50552</strong></h5>
+                </div>
             </div>
-            <div id="text-header" class="col-md-9">
-                <h3 class="kablogo">PEMERINTAH KABUPATEN SEMARANG</h3>
-                <h1 class="keclogo"><strong>KECAMATAN BERGAS</strong></h1>
-                <h6 class="alamatlogo">Jl. Soekarno-Hatta, No. 68, Telepon/Faximile (0298) 523024</h6>
-                <h5 class="kodeposlogo"><strong>BERGAS 50552</strong></h5>
-            </div>
-        </div>
-    </header>
+        </header>
 
-    <div class="container">
-        <hr class="garis1"/>
+        <div class="container">
+            <hr class="garis1"/>
 
+            <table border="1" id="table1">
+                <thead>
 
-        <table border="1" id="table1">
-            <thead>
+                    <tr>
+                            <th align="center" width="1%" rowspan="2"> NO </th>
+                            <th colspan="2"> BARANG</th>
+                            <th colspan="3"> TRANSAKSI</th>
+                            <th rowspan="2">TOTAL</th>
+                    </tr>
 
-                <tr>
-                        <th align="center" width="1%" rowspan="2"> NO </th>
-                        <th colspan="2"> BARANG</th>
-                        <th colspan="3"> TRANSAKSI</th>
-                        <th rowspan="2">TOTAL</th>
-                </tr>
+                    <tr>
+                        <th>KODE BARANG</th>
+                        <th>BARANG</th>
+                        <th>STOK AWAL</th>
+                        <th>JML MASUK</th>
+                        <th>JML KELUAR</th>
 
-                <tr>
-                    <th>KODE BARANG</th>
-                    <th>BARANG</th>
-                    <th>STOK AWAL</th>
-                    <th>JML MASUK</th>
-                    <th>JML KELUAR</th>
+                    </tr>
 
-                </tr>
+                </thead>
 
+                <thead>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </thead>
 
-            </thead>
-            <thead>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
-            </thead>
-            <tbody>
-                <?php $datas = BarangModel::leftJoin('tbl_jenisbarang', 'tbl_jenisbarang.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
-                    ->leftJoin('tbl_satuan', 'tbl_satuan.satuan_id', '=', 'tbl_barang.satuan_id')
-                    ->leftJoin('tbl_merk', 'tbl_merk.merk_id', '=', 'tbl_barang.merk_id')
-                    ->leftJoin('tbl_barangmasuk', 'tbl_barangmasuk.barang_kode', '=', 'tbl_barang.barang_kode')
-                    ->whereBetween('bm_tanggal', [$tglawal, $tglakhir])
-                    ->orderBy('barang_id', 'DESC')->get();
-                ?>
-
-
-                @php $no=1; @endphp
-                @foreach($datas as $d)
-                    <?php
-                        if($tglawal == ''){
-                            $jmlmasuk = BarangmasukModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangmasuk.barang_kode')->leftJoin('tbl_supplier', 'tbl_supplier.supplier_id', '=', 'tbl_barangmasuk.supplier_id')->where('tbl_barangmasuk.barang_kode', '=', $d->barang_kode)->sum('tbl_barangmasuk.bm_jumlah');
-                        }else{
-                            $jmlmasuk = BarangmasukModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangmasuk.barang_kode')
-                            ->leftJoin('tbl_supplier', 'tbl_supplier.supplier_id', '=', 'tbl_barangmasuk.supplier_id')
-                            ->where('tbl_barangmasuk.barang_kode', '=', $d->barang_kode)
-                            ->whereBetween('bm_tanggal', [$tglawal, $tglakhir])
-                            ->sum('tbl_barangmasuk.bm_jumlah');
-                        }
-
-                        if ($tglawal != '') {
-                            $jmlkeluar = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')->whereBetween('bk_tanggal', [$tglawal, $tglakhir])->where('tbl_barangkeluar.barang_kode', '=', $d->barang_kode)->sum('tbl_barangkeluar.bk_jumlah');
-                        } else {
-                            $jmlkeluar = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')->where('tbl_barangkeluar.barang_kode', '=', $d->barang_kode)->sum('tbl_barangkeluar.bk_jumlah');
-                        }
-
-                        $totalStok = $d->barang_stok + ($jmlmasuk-$jmlkeluar);
+                <tbody>
+                    <?php $datas = BarangModel::leftJoin('tbl_jenisbarang', 'tbl_jenisbarang.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
+                        ->leftJoin('tbl_satuan', 'tbl_satuan.satuan_id', '=', 'tbl_barang.satuan_id')
+                        ->leftJoin('tbl_merk', 'tbl_merk.merk_id', '=', 'tbl_barang.merk_id')
+                        ->leftJoin('tbl_barangmasuk', 'tbl_barangmasuk.barang_kode', '=', 'tbl_barang.barang_kode')
+                        ->whereBetween('bm_tanggal', [$tglawal, $tglakhir])
+                        ->orderBy('barang_id', 'DESC')->get();
                     ?>
 
 
-                    <tr>
-                        <td align="center">{{$no++}}</td>
-                        <td>{{$d->barang_kode}}</td>
-                        <td>{{$d->barang_nama}}</td>
-                        <td align="center">{{$d->barang_stok}}</td>
-                        <td align="center">{{$jmlmasuk}}</td>
-                        <td align="center">{{$jmlkeluar}}</td>
-                        <td align="center">{{$totalStok}}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    @php $no=1; @endphp
+                    @foreach($datas as $d)
+                        <?php
+                            if($tglawal == ''){
+                                $jmlmasuk = BarangmasukModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangmasuk.barang_kode')->leftJoin('tbl_supplier', 'tbl_supplier.supplier_id', '=', 'tbl_barangmasuk.supplier_id')->where('tbl_barangmasuk.barang_kode', '=', $d->barang_kode)->sum('tbl_barangmasuk.bm_jumlah');
+                            }else{
+                                $jmlmasuk = BarangmasukModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangmasuk.barang_kode')
+                                ->leftJoin('tbl_supplier', 'tbl_supplier.supplier_id', '=', 'tbl_barangmasuk.supplier_id')
+                                ->where('tbl_barangmasuk.barang_kode', '=', $d->barang_kode)
+                                ->whereBetween('bm_tanggal', [$tglawal, $tglakhir])
+                                ->sum('tbl_barangmasuk.bm_jumlah');
+                            }
 
+                            if ($tglawal != '') {
+                                $jmlkeluar = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')->whereBetween('bk_tanggal', [$tglawal, $tglakhir])->where('tbl_barangkeluar.barang_kode', '=', $d->barang_kode)->sum('tbl_barangkeluar.bk_jumlah');
+                            } else {
+                                $jmlkeluar = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')->where('tbl_barangkeluar.barang_kode', '=', $d->barang_kode)->sum('tbl_barangkeluar.bk_jumlah');
+                            }
+
+                            $totalStok = $d->barang_stok + ($jmlmasuk-$jmlkeluar);
+                        ?>
+
+
+                        <tr>
+                            <td align="center">{{$no++}}</td>
+                            <td>{{$d->barang_kode}}</td>
+                            <td>{{$d->barang_nama}}</td>
+                            <td align="center">{{$d->barang_stok}}</td>
+                            <td align="center">{{$jmlmasuk}}</td>
+                            <td align="center">{{$jmlkeluar}}</td>
+                            <td align="center">{{$totalStok}}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+
+            </table>
+
+        </div>
     </div>
-</div>
 
 </body>
 
