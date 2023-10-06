a5ded30fed005dce757fc1ecc17e6ee5b205b1c1
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-06 14:21:21 +0700

    Update LapStokBarangController.php

--- app/Http/Controllers/Admin/LapStokBarangController.php
+++ app/Http/Controllers/Admin/LapStokBarangController.php
@@ -34,6 +34,7 @@ class LapStokBarangController extends Controller
             $data['data'] = BarangModel::leftJoin('tbl_jenisbarang', 'tbl_jenisbarang.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
             ->leftJoin('tbl_satuan', 'tbl_satuan.satuan_id', '=', 'tbl_barang.satuan_id')
             ->leftJoin('tbl_merk', 'tbl_merk.merk_id', '=', 'tbl_barang.merk_id')
+            ->leftJoin('tbl_barangmasuk', 'tbl_barangmasuk.barang_kode', '=', 'tbl_barang.barang_kode')
             ->orderBy('barang_id', 'DESC')->get();
         }
 
