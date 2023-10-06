54611ac778f35eb3721dfc22879f996ea0fd0c04
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-06 10:53:11 +0700

    Update print.blade.php

--- resources/views/Admin/Laporan/StokBarang/print.blade.php
+++ resources/views/Admin/Laporan/StokBarang/print.blade.php
@@ -218,7 +218,7 @@ use Carbon\Carbon;
             <thead>
                 <tr>
                     <th rowspan="2"width="1%" > NO </th>
-                    <th rowspan="2"width="5%" > TANGGAl </th>
+                    <th rowspan="2"width="5%" > TANGGAL </th>
                     <th rowspan="2"width="5%" > NO./TGL. SURAT DASAR PENERIMAAN/PENGELUARAN </th>
                     <th rowspan="2"> URAIAN </th>
                     <th colspan="3"> BARANG-BARANG </th>
