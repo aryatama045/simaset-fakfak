b2e94ffa552b805a35ed4a429a29fa9bf1014aed
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-07 12:18:29 +0700

    Update LapStokBarangController.php

--- app/Http/Controllers/Admin/LapStokBarangController.php
+++ app/Http/Controllers/Admin/LapStokBarangController.php
@@ -31,14 +31,6 @@ class LapStokBarangController extends Controller
             ->whereBetween('bm_tanggal', [$request->tglawal, $request->tglakhir])
             ->groupBy('tbl_barangmasuk.barang_kode')
             ->orderBy('tbl_barangmasuk.bm_tanggal', 'DESC')->get();
-
-            $data['data'] = $data['data']->groupBy([
-                'bm_tanggal',
-                function ($item) {
-                    return $item['jenisbarang_nama'];
-                },
-            ], $preserveKeys = true);
-
         } else {
             $data['data'] = BarangModel::leftJoin('tbl_jenisbarang', 'tbl_jenisbarang.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
             ->leftJoin('tbl_kategori', 'tbl_kategori.kategori_id', '=', 'tbl_barang.kategori_id')
@@ -46,16 +38,16 @@ class LapStokBarangController extends Controller
             ->leftJoin('tbl_merk', 'tbl_merk.merk_id', '=', 'tbl_barang.merk_id')
             ->leftJoin('tbl_barangmasuk', 'tbl_barangmasuk.barang_kode', '=', 'tbl_barang.barang_kode')
             ->orderBy('barang_id', 'DESC')->get();
-
-            $data['data'] = $data['data']->groupBy([
-                function ($item) {
-                    return $item['jenisbarang_nama'];
-                },
-            ], $preserveKeys = true);
         }
 
         // $data['data'] = $data['data']->groupBy('jenisbarang_nama');
 
+        $data['data'] = $data['data']->groupBy([
+            'bm_tanggal',
+            function ($item) {
+                return $item['jenisbarang_nama'];
+            },
+        ], $preserveKeys = true);
 
         $data["title"] = "Print Stok Barang";
         $data['web'] = WebModel::first();
