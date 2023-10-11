3a709ac0f713f116bbbcb5390f5df685d7a6ed87
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-11 14:38:36 +0700

    Update Spk.php

--- app/Pdf/Spk.php
+++ app/Pdf/Spk.php
@@ -111,6 +111,47 @@ class Spk extends Fpdf
 
         }
 
+        function hari_tanggal_tahun($waktu)
+        {
+            $hari_array = array(
+                'Minggu',
+                'Senin',
+                'Selasa',
+                'Rabu',
+                'Kamis',
+                'Jumat',
+                'Sabtu'
+            );
+            $hr = date('w', strtotime($waktu));
+            $hari = $hari_array[$hr];
+            $tanggal = date('j', strtotime($waktu));
+            $bulan_array = array(
+                1 => 'Januari',
+                2 => 'Februari',
+                3 => 'Maret',
+                4 => 'April',
+                5 => 'Mei',
+                6 => 'Juni',
+                7 => 'Juli',
+                8 => 'Agustus',
+                9 => 'September',
+                10 => 'Oktober',
+                11 => 'November',
+                12 => 'Desember',
+            );
+            $bl = date('n', strtotime($waktu));
+            $bulan = $bulan_array[$bl];
+            $spell_tahun = date('Y', strtotime($waktu));
+            $tahun = $this->terbilang($spell_tahun)
+            $jam = date( 'H:i:s', strtotime($waktu));
+
+            //untuk menampilkan hari, tanggal bulan tahun jam
+            //return "$hari, $tanggal $bulan $tahun $jam";
+
+            //untuk menampilkan hari, tanggal bulan tahun
+            return "$hari, $tanggal $bulan $tahun";
+        }
+
     #------ END ---------------------------------------------------
 
 
@@ -249,7 +290,8 @@ class Spk extends Fpdf
             // $this->MultiCell(175,6,$this->header[0]->spk_header, 0);
 
             $hari = date('D', $this->header[0]->spk_tanggal);
-            $this->MultiCell(175,6,'Pada hari ini ' .hari_ini($hari). ' Tanggal  Empat Bulan Maret Tahun Dua Ribu Dua Puluh Dua, yang bertanda tangan dibawah ini :', 0);
+
+            $this->MultiCell(175,6,'Pada hari ini ' .$this->hari_ini($hari). ' Tanggal  Empat Bulan Maret Tahun Dua Ribu Dua Puluh Dua, yang bertanda tangan dibawah ini :', 0);
 
             $this->Ln(4);
             $this->setFont('Times','',11);
