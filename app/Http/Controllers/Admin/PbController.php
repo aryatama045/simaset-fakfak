e50c6400341ff5d0c5853632d58c59670dfaaeae
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-11 15:01:49 +0700

    Update PbController.php

--- app/Http/Controllers/Admin/PbController.php
+++ app/Http/Controllers/Admin/PbController.php
@@ -228,7 +228,7 @@ class PbController extends Controller
 
             $myPdf = new Nota($output);
 
-            $myPdf->Output('I', "Nota-pesan(".$data_header->no_dok.").pdf", true);
+            $myPdf->Output('I', "Nota-pesan(".$output['header'][0]->no_dok.").pdf", true);
 
             exit;
         }
