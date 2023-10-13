50532bf0b70a6355d2450aab1f321addee7e7eaa
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-13 16:55:58 +0700

    Update print.blade.php

--- resources/views/Admin/Laporan/HabisPakai/print.blade.php
+++ resources/views/Admin/Laporan/HabisPakai/print.blade.php
@@ -322,7 +322,7 @@ use Carbon\Carbon;
                                 <td style="text-align:left; font-size:12;">{{$d->barang_nama}}</td>
                                 <td> {{$d->merk_nama}} </td>
                                 <td>{{Carbon::parse($d->bm_tanggal)->translatedFormat('Y')}}</td>
-                                <td>{{$bm_jumlah}}</td>
+                                <td>{{$d->bm_jumlah}}</td>
                                 <td> </td>
                                 <!-- <td align="center">{{$jmlmasuk}}</td>
                                 <td align="center">{{$jmlkeluar}}</td>
