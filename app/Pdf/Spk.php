7bb7a74e3821e98012075588404a5962903c221f
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-15 20:43:05 +0700

    Update Spk.php

--- app/Pdf/Spk.php
+++ app/Pdf/Spk.php
@@ -502,9 +502,9 @@ class Spk extends Fpdf
         $this->Ln(2);
         $this->cell(10,1,'NO',0,0,'L');
         $this->cell(45,1,'URAIAN',0,0,'L');
-        $this->cell(35,1,'SISA STOK',0,0,'C');
+        $this->cell(35,1,'BANYAKNYA',0,0,'C');
         $this->cell(40,1,'HARGA SATUAN',0,0,'L');
-        $this->cell(40,1,'JUMLAH HARGA',0,0,'L');
+        $this->cell(40,1,'JUMLAH',0,0,'L');
         $this->Ln(2);
         $this->Ln(3);
         $this->Line(11,$this->GetY(),195,$this->GetY());
@@ -520,13 +520,13 @@ class Spk extends Fpdf
             }
 
 
-            $jumlah_harga = $value->stok * $value->barang_harga;
+            $jumlah_harga = $value->pb_jumlah * $value->barang_harga;
             $this->setFont('Times','',10);
             $this->cell(10,1,$no++,0,0,'L');
             $this->cell(45,1,$value->barang_nama,0,0,'L');
-            $this->cell(35,1,$value->stok,0,0,'C');
+            $this->cell(35,1,$value->pb_jumlah,0,0,'C');
             $this->cell(40,1,number_format($value->barang_harga, 0),0,0,'L');
-            $this->cell(40,1,number_format($jumlah_harga, 0),0,0,'L');
+            $this->cell(40,1,rupiah($jumlah_harga),0,0,'L');
             $this->Ln(4);
         }
         $this->setFont('Arial','B',7);
