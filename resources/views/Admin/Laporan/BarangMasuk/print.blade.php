a372da63ff214772c2a837847290a44f45b0def0
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-17 14:27:07 +0700

    Update print.blade.php

--- resources/views/Admin/Laporan/BarangMasuk/print.blade.php
+++ resources/views/Admin/Laporan/BarangMasuk/print.blade.php
@@ -24,35 +24,51 @@ use Carbon\Carbon;
 
     <title>{{$title}}</title>
 
+
     <style>
+        /* Default */
         * {
-            font-family: Arial, Helvetica, sans-serif;
+            font-family: Times, Helvetica, sans-serif;
+        }
+
+        @media print{
+            @page {
+                size: landscape;
+            }
         }
 
         #table1 {
             border-collapse: collapse;
             width: 100%;
-            margin-top: 32px;
+            margin-top: 20px;
         }
 
         #table1 td,
         #table1 th {
             border: 1px solid #ddd;
             padding: 8px;
+            text-align: center;
         }
 
         #table1 th {
-            padding-top: 12px;
-            padding-bottom: 12px;
+            padding-top: 4px;
+            padding-bottom: 4px;
             color: black;
             font-size: 12px;
         }
 
-        #table1 td {
-            font-size: 11px;
+        #thbottom th {
+            padding-top: 2px !important;
+            padding-bottom: 2px !important;
+            color: black;
+            font-size: 10px !important;
         }
 
-        .font-medium {
+        #table1 td {
+            font-size: 12px;
+        }
+
+        /* .font-medium {
             font-weight: 500;
         }
 
@@ -64,8 +80,85 @@ use Carbon\Carbon;
             display: flex;
             align-items: flex-start;
             margin-top: 32px;
+        } */
+        /* --- END Default --- */
+
+
+        h1,h3,h5,h6{
+            text-align:center;
+            /* padding-right:200px; */
+            margin:5px;
+        }
+
+
+        .row{
+            /* margin-top: 50px; */
+            width:100%;
+        }
+
+        #kop-surat{
+            margin-top: 50px;
+        }
+
+        .keclogo{
+            font-size:1.8rem;
+        }
+
+        .kablogo{
+            font-size:1.8rem;
+        }
+
+        .alamatlogo{
+            font-size:1em;
+        }
+
+        .kodeposlogo{
+            font-size:1em;
+        }
+
+        #tls{
+            text-align:right;
+        }
+
+        .alamat-tujuan{
+            margin-left:50%;
+        }
+
+        .garis1{
+            border-top:3px solid black;
+            height: 2px;
+            border-bottom:1px solid black;
+            margin-top: 6px;
+        }
+
+        #logo{
+            margin: auto;
+            margin-left: 35%;
+            margin-right: auto;
+        }
+
+        #tempat-tgl{
+            margin-left:120px;
+        }
+
+        #ttd{
+            margin-top:25px;
+        }
+
+        #tanggal-pengurus{
+            text-align:center;
+        }
+
+        #pengurus{
+            text-align:center;
+        }
+
+        #nama-pengurus{
+            margin-top:60px;
+            text-align:center;
         }
     </style>
+
 
 </head>
 
@@ -92,15 +185,45 @@ use Carbon\Carbon;
     <table border="1" id="table1">
         <thead>
             <tr>
-                <th align="center" width="1%">NO</th>
-                <th>TGL MASUK</th>
-                <th>KODE BRG MASUK</th>
-                <th>KODE BARANG</th>
-                <th>SUPPLIER</th>
-                <th>BARANG</th>
-                <th>JML MASUK</th>
+                <th rowspan="3" width="1%" > NO </th>
+                <th rowspan="3" width="5%" > TANGGAL </th>
+                <th rowspan="3" width="5%" > NAMA TOKO </th>
+                <th rowspan="3" width="15%"> URAIAN </th>
+                <th colspan="2"> DOKUMEN FAKTUR </th>
+                <th rowspan="3"> NAMA BARANG</th>
+                <th colspan="3"> BANYAK NYA </th>
+                <th rowspan="3"> HARGA SATUAN </th>
+                <th rowspan="3"> JUMLAH HARGA </th>
+                <th colspan="2"> BUKTI PENERIMAAN </th>
+                <th rowspan="3"> KET </th>
             </tr>
+
+            <tr>
+                <th rowspan="2">NOMOR</th>
+                <th rowspan="2">TANGGAL</th>
+
+                <th colspan="2"> B.A PENERIMAAN </th>
+
+                <th>SISA</th>
+                <th>BERTAMBAH</th>
+                <th>BERKURANG</th>
+                <th>SISA</th>
+            </tr>
+
+            <tr>
+                <th>NOMOR</th>
+                <th>TANGGAL</th>
+            </tr>
+
+            <tr id="thbottom">
+                @for($x=1; $x <= 12; $x++)
+                <th>{{$x}}</th>
+                @endfor
+            </tr>
+
         </thead>
+
+
         <tbody>
             @php $no=1; @endphp
             @foreach($data as $d)
