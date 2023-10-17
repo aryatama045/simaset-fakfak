084cde1d1b56c56841ffd3f69c95f065a7477c45
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-17 14:42:22 +0700

    Update print.blade.php

--- resources/views/Admin/Laporan/BarangMasuk/print.blade.php
+++ resources/views/Admin/Laporan/BarangMasuk/print.blade.php
@@ -206,7 +206,7 @@ use Carbon\Carbon;
                 <tr>
                     <th rowspan="3" width="1%" > NO </th>
                     <th rowspan="3" width="5%" > TANGGAL </th>
-                    <th rowspan="3" width="5%" > NAMA TOKO </th>
+                    <th rowspan="3" width="15%" > NAMA TOKO </th>
                     <th colspan="2"> DOKUMEN FAKTUR </th>
                     <th rowspan="3" width="20%"> NAMA BARANG</th>
                     <th rowspan="3" width="3%"> BANYAK NYA </th>
