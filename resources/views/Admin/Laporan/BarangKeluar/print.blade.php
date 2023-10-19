7d34a3dec73a90884bd7763543aaed2c7c2e51e3
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-19 14:20:39 +0700

    Update print.blade.php

--- resources/views/Admin/Laporan/BarangKeluar/print.blade.php
+++ resources/views/Admin/Laporan/BarangKeluar/print.blade.php
@@ -24,35 +24,55 @@ use Carbon\Carbon;
 
     <title>{{$title}}</title>
 
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:400,300|Raleway:300,400,900,700italic,700,300,600">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"  />
+
+
     <style>
+        /* Default */
         * {
-            font-family: Arial, Helvetica, sans-serif;
+            font-family: Times, Helvetica, sans-serif;
+        }
+
+        @media print{
+            @page {
+                size: landscape;
+            }
         }
 
         #table1 {
             border-collapse: collapse;
             width: 100%;
-            margin-top: 32px;
+            margin-top: 20px;
         }
 
         #table1 td,
         #table1 th {
             border: 1px solid #ddd;
             padding: 8px;
+            text-align: center;
         }
 
         #table1 th {
-            padding-top: 12px;
-            padding-bottom: 12px;
+            padding-top: 4px;
+            padding-bottom: 4px;
             color: black;
             font-size: 12px;
         }
 
-        #table1 td {
-            font-size: 11px;
+        #thbottom th {
+            padding-top: 2px !important;
+            padding-bottom: 2px !important;
+            color: black;
+            font-size: 10px !important;
         }
 
-        .font-medium {
+        #table1 td {
+            font-size: 12px;
+        }
+
+        /* .font-medium {
             font-weight: 500;
         }
 
@@ -64,6 +84,86 @@ use Carbon\Carbon;
             display: flex;
             align-items: flex-start;
             margin-top: 32px;
+        } */
+        /* --- END Default --- */
+
+
+        h1,h3,h5,h6{
+            text-align:center;
+            /* padding-right:200px; */
+            margin:5px;
+        }
+
+
+        .row{
+            /* margin-top: 50px; */
+            width:100%;
+        }
+
+        .form-group{
+            margin-bottom: 0px;
+        }
+
+        #kop-surat{
+            margin-top: 50px;
+        }
+
+        .keclogo{
+            font-size:1.8rem;
+        }
+
+        .kablogo{
+            font-size:1.8rem;
+        }
+
+        .alamatlogo{
+            font-size:1em;
+        }
+
+        .kodeposlogo{
+            font-size:1em;
+        }
+
+        #tls{
+            text-align:right;
+        }
+
+        .alamat-tujuan{
+            margin-left:50%;
+        }
+
+        .garis1{
+            border-top:3px solid black;
+            height: 2px;
+            border-bottom:1px solid black;
+            margin-top: 6px;
+        }
+
+        #logo{
+            margin: auto;
+            margin-left: 35%;
+            margin-right: auto;
+        }
+
+        #tempat-tgl{
+            margin-left:120px;
+        }
+
+        #ttd{
+            margin-top:25px;
+        }
+
+        #tanggal-pengurus{
+            text-align:center;
+        }
+
+        #pengurus{
+            text-align:center;
+        }
+
+        #nama-pengurus{
+            margin-top:60px;
+            text-align:center;
         }
     </style>
 
@@ -71,75 +171,125 @@ use Carbon\Carbon;
 
 <body onload="window.print()">
 
-    <center>
-        @if($web->web_logo == '' || $web->web_logo == 'default.png')
-        <img src="{{url('/assets/default/web/default.png')}}" width="80px" alt="">
-        @else
-        <img src="{{asset('storage/web/' . $web->web_logo)}}" width="80px" alt="">
-        @endif
-    </center>
+    <div class="container">
+        <header>
+            <div id="kop-surat" class="row">
+                <div class="col-md-2 col-sm-2 col-xs-2">
+                    <!-- <img id="logo" src="https://getasanbersinar.files.wordpress.com/2016/02/logo-kabupaten-semarang-jawa-tengah.png" width="140" height="160" /> -->
 
-    <center>
-        <h1 class="font-medium">Laporan Barang Keluar</h1>
-        @if($tglawal == '')
-        <h4 class="font-medium">Semua Tanggal</h4>
-        @else
-        <h4 class="font-medium">{{Carbon::parse($tglawal)->translatedFormat('d F Y')}} - {{Carbon::parse($tglakhir)->translatedFormat('d F Y')}}</h4>
-        @endif
-    </center>
+                    @if($web->web_logo == '' || $web->web_logo == 'default.png')
+                    <img id="logo" src="{{url('/assets/default/web/default.png')}}" width="85" height="90" alt="">
+                    @else
+                    <img id="logo" src="{{url('/assets/default/web/default.png')}}" width="85" height="90" alt="">
+                    @endif
+                </div>
+                <div class="col-md-10 col-sm-10 col-xs-10">
+                    <h3 class="kablogo"> <strong>{{ $web->header_1 }}</strong></h3>
+                    <h1 class="keclogo"><strong> {{ $web->header_2 }} </strong></h1>
+                    <h6 class="alamatlogo"> {{ $web->alamat }} </h6>
+                    <!-- <h5 class="kodeposlogo"><strong>BERGAS 50552</strong></h5> -->
+                </div>
+            </div>
+        </header>
+
+        <hr class="garis1"/>
+
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <h1 class="keclogo"><strong> Laporan Penerimaan
+                    <?php if($tglawal != ''){ ?>
+                        {{Carbon::parse($tglawal)->translatedFormat('d F Y')}} - {{Carbon::parse($tglakhir)->translatedFormat('d F Y')}}
+                    <?php }else{ ?> - Seluruh Barang <?php } ?></strong></h1>
+            </div>
+        </div>
+
+        <div id="alamat" class="row">
+            <div id="lampiran" class="col-md-6">
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">SKPD</label>
+                    <div class="col-sm-9">
+                    : SEKRETARIAT DPRD
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">KABUPATEN</label>
+                    <div class="col-sm-9">
+                    : FAKFAK
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-3 col-form-label">KOTA</label>
+                    <div class="col-sm-9">
+                    : FAKFAK
+                    </div>
+                </div>
+            </div>
+            <!-- <div id="tgl-srt" class="col-md-6">
+                <p id="tls">Bergas, 30 April 2018</p>
+
+                <p class="alamat-tujuan">Kepada Yth. :<br />
+                Kepala Desa</p>
+                <p class="alamat-tujuan">se - Kecamatan Bergas
+                </p>
+            </div> -->
+        </div>
 
 
-    <table border="1" id="table1">
-        <thead>
-            <tr>
-                <th rowspan="3" width="1%" > NO </th>
-                <th rowspan="3" width="7%" > TANGGAL </th>
-                <th rowspan="3" width="15%" > NAMA TOKO </th>
-                <th colspan="2"> DOKUMEN FAKTUR </th>
-                <th rowspan="3" width="30%"> NAMA BARANG</th>
-                <th rowspan="3" width="3%"> BANYAK NYA </th>
-                <th rowspan="3" width="3%"> HARGA SATUAN </th>
-                <th rowspan="3" width="3%"> JUMLAH HARGA </th>
-                <th colspan="2"> BUKTI PENERIMAAN </th>
-                <th rowspan="3"> KET </th>
-            </tr>
+        <table border="1" id="table1">
+            <thead>
+                <tr>
+                    <th rowspan="3" width="1%" > NO </th>
+                    <th rowspan="3" width="7%" > TANGGAL </th>
+                    <th rowspan="3" width="15%" > NAMA TOKO </th>
+                    <th colspan="2"> DOKUMEN FAKTUR </th>
+                    <th rowspan="3" width="30%"> NAMA BARANG</th>
+                    <th rowspan="3" width="3%"> BANYAK NYA </th>
+                    <th rowspan="3" width="3%"> HARGA SATUAN </th>
+                    <th rowspan="3" width="3%"> JUMLAH HARGA </th>
+                    <th colspan="2"> BUKTI PENERIMAAN </th>
+                    <th rowspan="3"> KET </th>
+                </tr>
 
-            <tr>
-                <th rowspan="2" width="7%">NOMOR</th>
-                <th rowspan="2" width="7%">TANGGAL</th>
+                <tr>
+                    <th rowspan="2" width="7%">NOMOR</th>
+                    <th rowspan="2" width="7%">TANGGAL</th>
 
-                <th colspan="2"> B.A PENERIMAAN </th>
+                    <th colspan="2"> B.A PENERIMAAN </th>
 
-            </tr>
+                </tr>
 
-            <tr>
-                <th width="7%">NOMOR</th>
-                <th width="7%">TANGGAL</th>
-            </tr>
+                <tr>
+                    <th width="7%">NOMOR</th>
+                    <th width="7%">TANGGAL</th>
+                </tr>
 
-            <tr id="thbottom">
-                @for($x=1; $x <= 12; $x++)
-                <th>{{$x}}</th>
-                @endfor
-            </tr>
-        </thead>
+                <tr id="thbottom">
+                    @for($x=1; $x <= 12; $x++)
+                    <th>{{$x}}</th>
+                    @endfor
+                </tr>
+            </thead>
 
 
-        <tbody>
-            @php $no=1; @endphp
-            @foreach($data as $d)
-            <tr>
-                <td align="center">{{$no++}}</td>
-                <td>{{Carbon::parse($d->bk_tanggal)->translatedFormat('d F Y')}}</td>
-                <td>{{$d->bk_kode}}</td>
-                <td>{{$d->barang_kode}}</td>
-                <td>{{$d->barang_nama}}</td>
-                <td align="center">{{$d->bk_jumlah}}</td>
-                <td>{{$d->bk_tujuan}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+            <tbody>
+                @php $no=1; @endphp
+                @foreach($data as $d)
+                <tr>
+                    <td align="center">{{$no++}}</td>
+                    <td>{{Carbon::parse($d->bk_tanggal)->translatedFormat('d F Y')}}</td>
+                    <td>{{$d->bk_kode}}</td>
+                    <td>{{$d->barang_kode}}</td>
+                    <td>{{$d->barang_nama}}</td>
+                    <td align="center">{{$d->bk_jumlah}}</td>
+                    <td>{{$d->bk_tujuan}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
 
 </body>
 
