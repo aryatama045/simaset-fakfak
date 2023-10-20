01fec99cbff1e083900db225e1f504b577ff3124
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-20 14:29:06 +0700

    Update print.blade.php

--- resources/views/Admin/Laporan/BarangKeluar/print.blade.php
+++ resources/views/Admin/Laporan/BarangKeluar/print.blade.php
@@ -283,7 +283,9 @@ use Carbon\Carbon;
                                 <td align="center"></td> <!-- Total Harga -->
                                 <td></td>
                                 <td></td>
-                                <td>{{$tk}}</td><!-- Tgl Penyerahan -->
+                                <td>@if($tgl_dokumen != $tk)
+                                        {{$tk}}
+                                    @endif</td><!-- Tgl Penyerahan -->
                                 <td></td>
                             </tr>
 
