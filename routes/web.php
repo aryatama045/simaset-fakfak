f2270fc317698d137ea04c1a3cf92316203cbaa5
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-30 13:39:37 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -130,7 +130,7 @@ Route::group(['middleware' => 'userlogin'], function () {
         Route::post('/admin/barang/proses_hapus/{barang}', [BarangController::class, 'proses_hapus']);
         Route::post('/admin/barang/import_barang/', [BarangController::class, 'import_barang'])->name('barang.import');
 
-        Route::get('/admin/barang/export_barang/', [BarangController::class, 'export_barang'])->name('barang.export');
+        Route::post('/admin/barang/export_barang/', [BarangController::class, 'export_barang'])->name('barang.export');
     });
 
     Route::middleware(['checkRoleUser:/customer,menu'])->group(function () {
