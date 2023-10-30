d81541196b0b7cc7dc61395c3ee0573b2a46802e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-10-30 13:50:53 +0700

    Update BarangController.php

--- app/Http/Controllers/Admin/BarangController.php
+++ app/Http/Controllers/Admin/BarangController.php
@@ -551,7 +551,7 @@ class BarangController extends Controller
 
     function export_barang($type)
     {
-        $data = BarangModel::get();
+        $data = BarangModel::get()->toArray();
         dd($type, $data);
         return Excel::create('laravelcode', function($excel) use ($data) {
             $excel->sheet('mySheet', function($sheet) use ($data)
