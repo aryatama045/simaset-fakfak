c419c04a1aa61342743f57f86987952886225ccb
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-11-02 10:37:17 +0700

    Update BarangExport.php

--- app/Export/BarangExport.php
+++ app/Export/BarangExport.php
@@ -6,15 +6,28 @@ use App\Models\Admin\BarangModel;
 use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
 
 
-class BarangExport implements FromCollection,WithHeadings
+class BarangExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents
 {
     public function headings():array{
         return[
             'Nama Barang',
             'Stok',
             'Harga',
+        ];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class    => function(AfterSheet $event) {
+                $cellRange = 'A1:W1'; // All headers
+                $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(14);
+            },
         ];
     }
 
