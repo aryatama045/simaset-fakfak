822807419b308a7d9c586adac7e9321df4dcecb8
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-11-03 14:12:19 +0700

    Update BarangExport.php

--- app/Export/BarangExport.php
+++ app/Export/BarangExport.php
@@ -9,18 +9,11 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Events\AfterSheet;
-
+use PhpOffice\PhpSpreadsheet\Style\Border;
 
 class BarangExport implements FromCollection, WithHeadings, ShouldAutoSize, WithEvents
 {
-    public function headings():array{
-        return[
-            'No.',
-            'Nama Barang',
-            'Stok',
-            'Harga',
-        ];
-    }
+
 
     public function registerEvents(): array
     {
@@ -35,7 +28,7 @@ class BarangExport implements FromCollection, WithHeadings, ShouldAutoSize, With
         $styleArray = [
             'borders' => [
                 'outline' => [
-                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    'borderStyle' => Border::BORDER_THICK,
                     'color' => ['argb' => 'FFFF0000'],
                 ],
             ],
@@ -48,28 +41,60 @@ class BarangExport implements FromCollection, WithHeadings, ShouldAutoSize, With
     * @return \Illuminate\Support\Collection
     */
 
+    public function headings():array{
+        return[
+            'No.',
+            'Kode Barang',
+            'Nama Barang',
+            'Kategori',
+            'Jenis',
+            'Merk',
+            'Satuan',
+            'Stok',
+            'Harga',
+            'Crated At',
+            'Create By',
+            'Updated At',
+            'Update By'
+        ];
+    }
+
     public function collection()
     {
         $all_barang_data = [];
-        $data_barang = BarangModel::select('barang_nama', 'barang_stok', 'barang_harga', 'barang_id')
+        $data_barang = BarangModel::leftJoin('tbl_kategori as tk', 'tk.kategori_id','=', 'tbl_barang.kategori_id')
+                    ->leftJoin('tbl_jenisbarang as tjb', 'tjb.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
+                    ->leftJoin('tbl_merk as tm', 'tm.merk_id', '=', 'tbl_barang.merk_id')
+                    ->leftJoin('tbl_satuan as ts', 'ts.satuan_id', '=', 'tbl_barang.satuan_id')
+                    ->select('barang_nama', 'barang_stok', 'barang_harga', 'barang_id', 'tk.kategori_nama',
+                            'tjb.jenisbarang_nama', 'tm.merk_nama', 'ts.satuan_nama')
                     ->get();
 
         $no=1;
         foreach ($data_barang as $barang) {
             // $data= [];
-            // foreach ($barang->studentRecords as $record) {
-            //     if (moduleStatusCheck('University')) {
-            //         $data[] = $record->unFaculty->name.'('. $record->unDepartment->name .'),' ;
-            //     } else {
-            //         $data[] = $record->class->class_name." (". $record->section->section_name . ")";
-            //     }
-            // }
+                // foreach ($barang->studentRecords as $record) {
+                //     if (moduleStatusCheck('University')) {
+                //         $data[] = $record->unFaculty->name.'('. $record->unDepartment->name .'),' ;
+                //     } else {
+                //         $data[] = $record->class->class_name." (". $record->section->section_name . ")";
+                //     }
+                // }
             // $classSection = implode(', ', $data);
             $all_barang_data[] = [
                 $no++,
+                $barang->barang_kode,
                 $barang->barang_nama,
+                $barang->kategori_nama,
+                $barang->jenisbarang_nama,
+                $barang->merk_nama,
+                $barang->satuan,
                 $barang->barang_stok,
                 $barang->barang_harga,
+                '',
+                '',
+                '',
+                ''
 
             ];
         }
