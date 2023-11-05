3240e213eec79156e6cb96a444fbfdf7ae081078
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-11-05 15:56:11 +0700

    Update index.blade.php

--- resources/views/Admin/Barang/index.blade.php
+++ resources/views/Admin/Barang/index.blade.php
@@ -44,7 +44,7 @@
                 <h3 class="card-title">Data</h3>
                 @if($hakTambah > 0)
                 <div>
-                    <a class="modal-effect btn btn-default-light" href="barang/export_barang/xlsx" >Export <i class="fe fe-download"></i></a>
+                    <a class="btn btn-default-light" href="barang/export_barang/xlsx" target="_blank" >Export <i class="fe fe-download"></i></a>
                     <a class="modal-effect btn btn-success-light" onclick="generateID()" data-bs-effect="effect-super-scaled" data-bs-toggle="modal" href="#modalimport">Import <i class="fe fe-upload"></i></a>
                     <a class="modal-effect btn btn-primary-light" onclick="generateID()" data-bs-effect="effect-super-scaled" data-bs-toggle="modal" href="#modaldemo8">Tambah Data <i class="fe fe-plus"></i></a>
                 </div>
