c9a8b8c843a3072da4085160ffdc24b915ecce0e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-11-06 09:44:52 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -123,8 +123,8 @@ Route::group(['middleware' => 'userlogin'], function () {
     Route::middleware(['checkRoleUser:/barang,submenu'])->group(function () {
         // Barang
         Route::resource('/admin/barang', \App\Http\Controllers\Admin\BarangController::class);
-        // Route::resource('/admin/barang/history', BarangController::class, 'history');
-        Route::get('/admin/barang/history', 'App\Http\Controllers\Admin\BarangController@history');
+        Route::resource('/admin/barang/history', BarangController::class, 'history');
+        // Route::get('/admin/barang/history', 'App\Http\Controllers\Admin\BarangController@history');
         Route::get('/admin/barang/showhistory/', [BarangController::class, 'showhistory'])->name('barang.gethistory');
         Route::get('/admin/barang/show/', [BarangController::class, 'show'])->name('barang.getbarang');
         Route::post('/admin/barang/proses_tambah/', [BarangController::class, 'proses_tambah'])->name('barang_tambah.store');
