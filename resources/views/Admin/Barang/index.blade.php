214fa209004a426afde78fb3500a94c3b725d5a8
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-11-06 11:09:03 +0700

    Update index.blade.php

--- resources/views/Admin/Barang/index.blade.php
+++ resources/views/Admin/Barang/index.blade.php
@@ -188,7 +188,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function () {
-    $('#tabs').tab();
+    $('#tab').tab();
 });
 </script>
 <script>
