ed9b47bc9cb9e9bba832eafa522651fbfea5e10d
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-08 13:25:14 +0700

    Update BarangHistoryController.php

--- app/Http/Controllers/Admin/BarangHistoryController.php
+++ app/Http/Controllers/Admin/BarangHistoryController.php
@@ -52,7 +52,7 @@ class BarangHistoryController extends Controller
             ->leftJoin('tbl_barang', 'tbl_barang_log.barang_id' , '=','tbl_barang.barang_id' )
             ->leftJoin('tbl_user', 'tbl_barang_log.user_id' , '=',  'tbl_user.user_id')
             ->select('tbl_barang_log.keterangan','tbl_barang_log.created_at', 'tbl_user.user_nmlengkap', 'tbl_barang.barang_kode','tbl_barang.barang_nama')
-            ->orderBy('tanggal', 'DESC')->get();
+            ->orderBy('created_at', 'DESC')->get();
 
             dd($data);
 
