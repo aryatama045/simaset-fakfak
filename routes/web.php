b0470473e6a84789d154ba6a95bad710bac8294a
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-08 11:20:11 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -124,8 +124,8 @@ Route::group(['middleware' => 'userlogin'], function () {
         // Barang
         Route::resource('/admin/barang', \App\Http\Controllers\Admin\BarangController::class);
 
-        Route::resource('/admin/datahistory', [\App\Http\Controllers\Admin\BarangController::class, 'datahistory']);
-        // Route::get('/admin/datahistory/', [BarangController::class, 'datahistory']);
+        // Route::resource('/admin/datahistory', [\App\Http\Controllers\Admin\BarangController::class, 'datahistory']);
+        Route::get('/admin/datahistory/', [BarangController::class, 'datahistory']);
 
         Route::get('/admin/barang/gethistory/', [BarangController::class, 'gethistory'])->name('barang.data-history');
         Route::get('/admin/barang/show/', [BarangController::class, 'show'])->name('barang.getbarang');
