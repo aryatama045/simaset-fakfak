1ccd10bdab660ccbaafe7ce4962a8234efc9be8d
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-08 10:40:30 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -123,9 +123,8 @@ Route::group(['middleware' => 'userlogin'], function () {
     Route::middleware(['checkRoleUser:/barang,submenu'])->group(function () {
         // Barang
         Route::resource('/admin/barang', \App\Http\Controllers\Admin\BarangController::class);
-        Route::get('/admin/datahistory/', [BarangController::class, 'datahistory']);
+        // Route::get('/admin/datahistory/', [BarangController::class, 'datahistory']);
 
-        Route::get('/admin/barang/gethistory/', [BarangController::class, 'gethistory'])->name('barang.gethistory');
         Route::get('/admin/barang/show/', [BarangController::class, 'show'])->name('barang.getbarang');
         Route::post('/admin/barang/proses_tambah/', [BarangController::class, 'proses_tambah'])->name('barang.store');
         Route::post('/admin/barang/bulk_delete/', [BarangController::class, 'bulk_delete'])->name('barang.bulk_delete');
@@ -138,6 +137,7 @@ Route::group(['middleware' => 'userlogin'], function () {
         Route::controller(BarangController::class)->group(function () {
             Route::prefix('/admin/barang')->group(function () {
                 Route::get('datahistory', 'showhistory')->name('barang.showHistory');
+                Route::get('/admin/barang/gethistory/', [BarangController::class, 'gethistory'])->name('barang.gethistory');
             });
         });
     });
