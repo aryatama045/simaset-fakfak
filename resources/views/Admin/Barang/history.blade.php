584af3ef5f83c7990e0a33ebbcdadbd172cf1d19
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-08 10:14:24 +0700

    Update history.blade.php

--- resources/views/Admin/Barang/history.blade.php
+++ resources/views/Admin/Barang/history.blade.php
@@ -132,7 +132,7 @@
             "pageLength": 10,
             lengthChange: true,
             "ajax": {
-                "url": "{{route('barang.getbarang')}}",
+                "url": "{{route('barang.gethistory')}}",
             },
             'columnDefs': [
                 {
