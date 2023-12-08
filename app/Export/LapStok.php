16bcedd31333195961c41fd32de2fd9da59775a1
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-08 16:42:19 +0700

    Update LapStok.php

--- app/Export/LapStok.php
+++ app/Export/LapStok.php
@@ -108,7 +108,7 @@ class LapStok implements FromCollection, WithHeadings, ShouldAutoSize, WithEvent
             }
 
             $bertambah = $jmlmasuk * $dh->barang_harga;
-            // $bertambah = number_format($bertambah,0,"",'.');
+            $bertambah = number_format($bertambah,0,"",'.');
             $berkurang = $jmlkeluar * $dh->barang_harga;
             $berkurang = number_format($berkurang,0,"",'.');
 
@@ -136,7 +136,7 @@ class LapStok implements FromCollection, WithHeadings, ShouldAutoSize, WithEvent
                 $sisa,
                 $ket,
             ];
-            dd($all_data);
+            // dd($all_data);
         }
 
         return collect($all_data);
