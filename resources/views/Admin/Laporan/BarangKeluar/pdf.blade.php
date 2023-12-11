fdf726ca0acf20bcc1bd94fa4768af133e032e9e
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-11 10:35:56 +0700

    Update pdf.blade.php

--- resources/views/Admin/Laporan/BarangKeluar/pdf.blade.php
+++ resources/views/Admin/Laporan/BarangKeluar/pdf.blade.php
@@ -24,8 +24,6 @@ use Carbon\Carbon;
 
     <title>{{$title}}</title>
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"  />
 
 
     <style>
