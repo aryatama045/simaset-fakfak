59f00c9cff2be5380fa16e492fbe175598fef741
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-11 14:41:54 +0700

    Update index.blade.php

--- resources/views/Admin/Laporan/HabisPakai/index.blade.php
+++ resources/views/Admin/Laporan/HabisPakai/index.blade.php
@@ -45,7 +45,7 @@
                         <div class="pull-right">
                             <a class="modal-effect btn btn-default-light" onclick="generateID()" data-bs-effect="effect-super-scaled" data-bs-toggle="modal" href="#modalexport">Export <i class="fe fe-download"></i></a>
 
-                            <!-- <button class="btn btn-primary-light" onclick="print()"><i class="fe fe-printer"></i> Print</button> -->
+                            <button class="btn btn-primary-light" onclick="print()"><i class="fe fe-printer"></i> Print</button>
                             <!-- <button class="btn btn-danger-light" onclick="pdf()"><i class="fa fa-file-pdf-o"></i> PDF</button> -->
                         </div>
                     </div>
