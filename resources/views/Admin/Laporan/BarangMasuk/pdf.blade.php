2b710abdf5fa10bda78b960bbbac615ae07e7af2
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-11 14:11:12 +0700

    Update pdf.blade.php

--- resources/views/Admin/Laporan/BarangMasuk/pdf.blade.php
+++ resources/views/Admin/Laporan/BarangMasuk/pdf.blade.php
@@ -19,10 +19,6 @@ use Carbon\Carbon;
     <title>{{$title}}</title>
 
     <link rel="stylesheet" href="{{url('/assets/css/pdf_bs1.css')}}" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"  />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" />
-
 
     <style>
         /* Default */
