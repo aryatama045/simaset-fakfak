34f8ac940ce2b1a3026669ff023c34e8b18f7fb7
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-11 14:22:38 +0700

    Update LapBarangMasukController.php

--- app/Http/Controllers/Admin/LapBarangMasukController.php
+++ app/Http/Controllers/Admin/LapBarangMasukController.php
@@ -78,7 +78,7 @@ class LapBarangMasukController extends Controller
         $data['web'] = WebModel::first();
         $data['tglawal'] = $request->tglawal;
         $data['tglakhir'] = $request->tglakhir;
-        $pdf = PDF::loadView('Admin.Laporan.BarangMasuk.pdf', $data)->setPaper('letter', 'landscape')->setOption('margin', 0);
+        $pdf = PDF::loadView('Admin.Laporan.BarangMasuk.pdf', $data)->setPaper('legal', 'landscape')->setOption('margin', 0);
 
         if($request->tglawal){
             return $pdf->download('lap-bm-'.$request->tglawal.'-'.$request->tglakhir.'.pdf');
