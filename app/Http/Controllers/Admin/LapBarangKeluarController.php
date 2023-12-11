cba713813a23ae3f299873c94da0742d5a2f9b3f
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-12-11 10:02:07 +0700

    Update LapBarangKeluarController.php

--- app/Http/Controllers/Admin/LapBarangKeluarController.php
+++ app/Http/Controllers/Admin/LapBarangKeluarController.php
@@ -66,29 +66,29 @@ class LapBarangKeluarController extends Controller
         //     $data['data'] = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')->orderBy('bk_id', 'DESC')->get();
         // }
 
-        if ($request->tglawal) {
-            $data['data'] = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')
-                            ->leftJoin('tbl_jenisbarang as jb', 'jb.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
-                            ->whereBetween('bk_tanggal', [$request->tglawal, $request->tglakhir])
-                            ->orderBy('bk_id', 'DESC')->get();
+        // if ($request->tglawal) {
+        //     $data['data'] = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')
+        //                     ->leftJoin('tbl_jenisbarang as jb', 'jb.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
+        //                     ->whereBetween('bk_tanggal', [$request->tglawal, $request->tglakhir])
+        //                     ->orderBy('bk_id', 'DESC')->get();
 
-            $data['data'] = $data['data']->groupBy([
-                'bk_tanggal',
-                function ($item) {
-                    return $item['jenisbarang_nama'];
-                },
-            ], $preserveKeys = true);
-        } else {
-            $data['data'] = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')
-                            ->leftJoin('tbl_jenisbarang as jb', 'jb.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
-                            ->orderBy('bk_id', 'DESC')->get();
-            $data['data'] = $data['data']->groupBy([
-                'bk_tanggal',
-                function ($item) {
-                    return $item['jenisbarang_nama'];
-                },
-            ], $preserveKeys = true);
-        }
+        //     $data['data'] = $data['data']->groupBy([
+        //         'bk_tanggal',
+        //         function ($item) {
+        //             return $item['jenisbarang_nama'];
+        //         },
+        //     ], $preserveKeys = true);
+        // } else {
+        //     $data['data'] = BarangkeluarModel::leftJoin('tbl_barang', 'tbl_barang.barang_kode', '=', 'tbl_barangkeluar.barang_kode')
+        //                     ->leftJoin('tbl_jenisbarang as jb', 'jb.jenisbarang_id', '=', 'tbl_barang.jenisbarang_id')
+        //                     ->orderBy('bk_id', 'DESC')->get();
+        //     $data['data'] = $data['data']->groupBy([
+        //         'bk_tanggal',
+        //         function ($item) {
+        //             return $item['jenisbarang_nama'];
+        //         },
+        //     ], $preserveKeys = true);
+        // }
 
         $data["title"] = "PDF Barang Masuk";
         $data['web'] = WebModel::first();
